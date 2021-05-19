4163e8dd374db8262ec8c765b35fff02d7642c0e
Tamás Hugyák <tahubu@outlook.com>
2021-05-19 15:01:37 +0200

    ci(release): use supercharge-bot user SSH key

--- release.sh
+++ release.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 # configure Git user and e-mail for the release commit
-git config user.name "Tamás Hugyák"
-git config user.email tahubu@outlook.com
+git config user.name "supercharge-bot"
+git config user.email balazs.kovacs+bot@supercharge.io
 
 # generate changelog and new version number then create a release commit
 npm run release
 
+# start SSH agent
+eval $(ssh-agent -t 60 -s)
+# add SSH key to push the release commit and tag with
+echo "${SUPERCHARGE_BOT_DEPLOY_KEY}" | ssh-add -
+
+# add github.com as known host -> SSH connect won't ask it
+mkdir -p ~/.ssh/
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 # push the release commit and the new tag to the upstream
-git push --follow-tags origin master
+git push --follow-tags git@github.com:team-supercharge/nest-amqp.git master
+
+# delete all manually added SSH keys
+ssh-add -D
+# stop SSH agent
+ssh-agent -k
+
+# remove the github.com known host
+rm ~/.ssh/known_hosts
 
 # build the production app
 npm run build:prod
