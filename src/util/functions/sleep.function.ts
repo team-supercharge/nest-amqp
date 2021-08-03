aa8d60c637989a6bf8248015752e72a20bc9c091
Marcell Huszti <marcell.huszti@supercharge.io>
2021-08-03 16:09:12 +0200

    test(sleep): add istanbul ignore

--- src/util/functions/sleep.function.ts
+++ src/util/functions/sleep.function.ts
@@ -3,6 +3,7 @@
  *
  * @param {number} ms Waiting time in milliseconds.
  */
+// istanbul ignore next: no need to test
 export async function sleep(ms: number): Promise<void> {
   return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
